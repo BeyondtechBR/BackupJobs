8f50645126cfce33f1edc79b05ff667f51be66a7
BeyondtechBR <67350718+BeyondtechBR@users.noreply.github.com>
2020-06-25 15:16:33 -0300

    Update mFunctions.psm1

--- mFunctions.psm1
+++ mFunctions.psm1
@@ -1,3 +1,14 @@
+########################################################
+# Nome arquivo : mFunctions.ps1
+# Versão       : 1.0
+# Data         : 25/06/2020 
+# Autor        : Alexandre Caneo
+# Objetivo     : Esse é um módulo que conterá várias funções
+# Funções      : 
+#  - Replace-SpecialChars : eliminar caracteres especiais da string
+########################################################  
+
+
 function Replace-SpecialChars {
     param(
         [string]$InputString,
